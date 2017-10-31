96a076e4e82ae4c98f8f5ad8e996affff4fb487f
Thomas Rzipa <t.rzipa@adrom.at>
2017-10-31 15:27:19 +0100

    Update to pug

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 var express = require('express')
-var multer  = require('multer')
+var multer = require('multer')
 var storage = multer.memoryStorage()
 var upload = multer({ storage: storage })
 
@@ -8,14 +8,22 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
-app.post('/upload', upload.single('file'), function(req, res){
-    var size = req.file.size;
-    res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify({ size: size }))
-    res.end()
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  var mainDomain = req.hostname.substr(req.hostname.indexOf('.') + 1);
+  var baseHome = req.protocol + '://' + mainDomain + ':' + port
+
+  res.render('index', { home: baseHome });
 });
 
+app.post('/upload', upload.single('file'), function (req, res) {
+  var size = req.file.size;
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify({ size: size }))
+  res.end()
+});
 
 app.listen(port, function () {
-  console.log('App listening on port '+port)
+  console.log('App listening on port ' + port)
 })
